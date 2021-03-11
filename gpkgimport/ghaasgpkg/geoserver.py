e6f8b4920688bce816dd16f72b4e8cf9ae6d2747
Daniel Vignoles <dvignoles@gmail.com>
2021-03-11 16:58:07 -0500

    distinguish geography only tables

--- gpkgimport/ghaasgpkg/geoserver.py
+++ gpkgimport/ghaasgpkg/geoserver.py
@@ -3,12 +3,18 @@ from geo.Geoserver import Geoserver
 def connect_geoserver(geoserver_url, user, password):
     return Geoserver(geoserver_url, user, password)
 
-def publish_geoserver_sqlview(geo, view_name, store_name, workspace):
+def publish_geoserver_sqlview(geo, view_name, store_name, workspace, geography=False):
     sql = 'SELECT * FROM {}'.format(view_name)
     name = view_name.split('.')[1].replace('+','-').replace('"','')
+
+    # geography tables PK distinction
+    key_col = 'sampleid'
+    if geography: 
+        key_col = 'id'
+
     geo.publish_featurestore_sqlview(name=name, store_name=store_name, sql=sql, key_column='sampleid', workspace=workspace)
     print(name)
 
-def publish_geoserver_sqlview_batch(geo, views_list, store_name, workspace):
+def publish_geoserver_sqlview_batch(geo, views_list, store_name, workspace, geography=False):
     for v in views_list:
-        publish_geoserver_sqlview(geo, v, store_name, workspace)
+        publish_geoserver_sqlview(geo, v, store_name, workspace, geography=geography)
