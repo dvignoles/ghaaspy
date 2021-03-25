135af7b59034dc1d936e7b2a973f024a6a0c6b64
Daniel Vignoles <dvignoles@gmail.com>
2021-03-25 20:55:51 +0000

    pgpass constructor and gdalstring conversion

--- gpkgimport/ghaasgpkg/postgres.py
+++ gpkgimport/ghaasgpkg/postgres.py
@@ -21,3 +21,50 @@ class PostgresDB:
         except Exception as err:
             print("psycopg2 connect() ERROR:", err)
             self.conn = None
+    
+    @classmethod
+    def from_pgpass(cls, idsubstring, pgpass=Path.home().joinpath('.pgpass').resolve()):
+        """Use postgres password file as source of postgres connection. See https://www.postgresql.org/docs/current/libpq-pgpass.html.
+        Entries are of form hostname:port:database:username:password
+
+
+        Args:
+            idsubstring (str): identifying substring ie database, hostname:port:database, hostname:port:databse:username
+         pgpass (Path, optional): Path of .pgpass file. Defaults to Path.home().joinpath('.pgpass').resolve().
+
+        Raises:
+            FileNotFoundError: if pgpass not valid 
+
+        Returns:
+            PostgresDB: class instance
+        """
+        if not pgpass.exists():
+            raise FileNotFoundError(".pgpass file not found at {}".format(pgpass))
+        
+        with open(pgpass, 'r') as f:
+            credentials = f.read().splitlines()
+        
+        matches = []
+        for c in credentials:
+            if idsubstring in c:
+                matches.append(c)
+        
+        assert(len(matches) == 1)
+        
+        host, port, db, user, password = matches[0].split(':')
+        return cls(database=db, user=user, password=password, host=host, port=port)
+    
+    @classmethod
+    def from_gdal_string(cls, gdal_pg):
+        pass
+
+    
+    def get_gdal_string(self):
+        """Return gdal driver format postgres connection string
+        https://gdal.org/drivers/vector/pg.html
+        PG:"dbname='databasename' host='addr' port='5432' user='x' password='y'"
+
+        Returns:
+            [type]: [description]
+        """
+        return '"dbname={} host={} port={} user={} password={}"'.format(self.database, self.host, self.port, self.user, self.password)
